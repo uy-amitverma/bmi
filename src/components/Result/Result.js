2d130d3a4e49640dc8ab731f1c3ac30682f2bfef
uy-amitverma <amit.verma@udyogyantra.com>
2022-01-05 19:26:31 +0530

    eod: ui updated

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,41 +1,78 @@
 import { useRef, useState } from "react";
 import styles from "./Result.module.css";
-import Webcam from "react-webcam";
 import img from "../../Images/newProject.png";
 import calculationGif from "../../Images/calculationGif.gif";
-import weightGif from "../../Images/weight.gif";
+import weightGif from "../../Images/weighImage.jpg";
 import clouds from "../../Images/cloud.jpeg";
+import weightCloud from "../../Images/cloudnewProject.png";
+import bmiScaleImg from "../../Images/bmi.jpeg";
 
-const Result = () => {
-  const sliderRef = useRef(null);
+const Result = (props) => {
+  const heightSliderRef = useRef(null);
+  const bmiSliderRef = useRef(null);
+  const { dataFound } = props;
+  const [sliderValue, setSliderValue] = useState(props.height);
   const sliderChange = () => {
-    console.log(sliderRef.current.value);
+    console.log(heightSliderRef.current.value);
+    setSliderValue(heightSliderRef.current.value);
   };
-
+  const bmiSliderChange = () => {
+    console.log(bmiSliderRef.current.value);
+    // setSliderValue(bmiSliderRef.current.value);
+  };
   return (
     <div className={styles["image-capture-wrapper"]}>
-      <img src={img} className={styles.measureImg} />
+      <img src={img} className={styles.measureImg} alt='height-measurement'/>
       <div className={styles["slider-container"]}>
         <input
           type="range"
           min={10}
           max={130}
-          value={109}
-          ref={sliderRef}
+          value={sliderValue}
+          ref={heightSliderRef}
           className={styles["slider"]}
           onChange={sliderChange}
         />
       </div>
-      <div className={styles["image-container"]}>
-        <img src={weightGif} className={styles.weightImg} />
-        <div className={styles.calculation}>
-          <span>Amit Verma</span>
-          <img src={clouds} className={styles.cloudImg} />
-          <img src={calculationGif} className={styles.calculationImg} />
+      <div className={styles["info-container"]}>
+        <div className={styles["bmi-section"]}>
+          <input
+            type="range"
+            min={16}
+            max={35}
+            ref={bmiSliderRef}
+            className={styles["bmi-slider"]}
+            onChange={bmiSliderChange}
+          />
+          <img className={styles.bmiScale} src={bmiScaleImg} alt="bmi scale" />
+        </div>
+        <div className={styles["name-weight-container"]}>
+          <div className={styles["weight-wrapper"]}>
+            {/* <span className={styles.weightText}>65Kg</span> */}
+            <img
+              src={weightCloud}
+              className={styles.weightCloud}
+              alt="weight"
+            />
+            <img
+              src={weightGif}
+              className={styles.weightImg}
+              alt="weightCartoonImage"
+            />
+          </div>
+
+          <div className={styles["name-wrapper"]}>
+            {/* <span>Srinivas Shyam Sundar Kasi Visveswara Rao Danaboyina</span> */}
+            <img src={clouds} className={styles.cloudImg} alt='name'/>
+            <img src={calculationGif} className={styles.calculationImg} alt='calculation'/>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
+Result.defaultProps = {
+  height: 102,
+};
 export default Result;
